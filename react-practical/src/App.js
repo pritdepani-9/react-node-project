ac61d62c006595e132a47aefc4f890f805088a7e
pritdepani-9 <pritpatidar9999@gmail.com>
2025-03-31 22:33:25 +0530

    Update App.js

--- react-practical/src/App.js
+++ react-practical/src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          here it is practical
+          
         </p>
         <a
           className="App-link"
